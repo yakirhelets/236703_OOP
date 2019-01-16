03c84d98cd908da4e85ab16577b0c6ef6193285e
alexbla2 <alexblago3@gmail.com>
2019-01-16 20:21:57 +0200

    wtf

--- main.cpp
+++ main.cpp
@@ -1,28 +1,35 @@
 #include <iostream>
+#include <assert.h>
 
-#include "part2/tests/Part2Main.h"
-#include "part1/tests/Part1Main.h"
+// #include "part2/tests/Part2Main.h"
+// #include "part1/tests/Part1Main.h"
 
-#include "Tests/PartOneTests/partOneMatrixSetGetTests.h"
-#include "Tests/PartOneTests/partOneMatrixTest.h"
-#include "Tests/PartOneTests/partOneUtilityTests.h"
+// #include "Tests/PartOneTests/partOneMatrixSetGetTests.h"
+// #include "Tests/PartOneTests/partOneMatrixTest.h"
+// #include "Tests/PartOneTests/partOneUtilityTests.h"
 
-#include "Tests/PartTwoTests/partTwoObserverTests.h"
-#include "Tests/PartTwoTests/partTwoSubjectTests.h"
+// #include "Tests/PartTwoTests/partTwoObserverTests.h"
+// #include "Tests/PartTwoTests/partTwoSubjectTests.h"
+
+#include "part1/Utilities.h"
 
 int main() {
-    part1Main();
-    part2Main();
+    typedef List<Int<1>> types;
+    static_assert(types::head::val ==  1, "Not the answer!");
+    //static_assert( typeid(types::head) == typeid(Int<1>));
+
+    // part1Main();
+    // part2Main();
     
     
-    //SERG Tests
-    utility_test();
-    get_set_matrix_test();
-    matrix_test();
+    // //SERG Tests
+    // utility_test();
+    // get_set_matrix_test();
+    // matrix_test();
     
-    //Part Two
-    observer_test();
-    subject_test();
+    // //Part Two
+    // observer_test();
+    // subject_test();
     
     
     return 0;
--- part1/Utilities.h
+++ part1/Utilities.h
@@ -5,34 +5,38 @@
 #ifndef INC_236703_HW5_UTILITIES_H
 #define INC_236703_HW5_UTILITIES_H
 
+
 template<typename T, typename... TT>
 struct List {
-    T head;
-    List<TT...> next;
-    int size = sizeof... (TT);
+    typedef T head;
+    typedef List<TT...> next;
+    //int size = sizeof... (TT);
 };
 
-template <typename T>
-class PrependList {
-    T type;
-    List<T> list;
-};
+// template <typename T>
+// class PrependList {
+//     T type;
+//     List<T> list;
+// };
 
-template <typename T>
-class ListGet {
-    int index;
-    List<T> list;
-};
+// template <typename T>
+// class ListGet {
+//     int index;
+//     List<T> list;
+// };
 
-template <typename T>
-class ListSet {
-    int index;
-    List<T> list;
-};
+// template <typename T>
+// class ListSet {
+//     int index;
+//     List<T> list;
+// };
 
-template <typename N>
+template <int>
+struct Int;
+
+template <int N>
 struct Int {
-    int value = N;
+    constexpr static int val = N;
 };
 
 #endif //INC_236703_HW5_UTILITIES_H
