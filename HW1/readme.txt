c70ad8e9922ae983ea2473dd5a5e7587288f637e
Yakir <yakir.helets@gmail.com>
2019-02-07 14:15:43 +0200

    Repo cleanup & Upload assignments PDF files

--- HW1/readme.txt
+++
@@ -1,2 +0,0 @@
-Alex Blagodarski 319259180 alex.b@campus.technion.ac.il
-Yakir Helets 305028441 yakir.helets@gmail.com
--- HW5/part2/tests/Part2Main.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "staff/Part2Examples.h"
-#include "my/MyTest.h"
-
-void part2Main() {
-    MyTest().test();
-    Part2Examples().mainPart2Examples();
-}
